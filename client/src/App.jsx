d97ae1c4d982cca229591a8bebb6dc9e20afd260
Leandro Kevin Monaco <monacoleandrokevin@gmail.com>
2025-08-04 20:25:22 -0300

    CambioAppjsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { GoogleLogin } from "@react-oauth/google";
-import axios from "axios";
 import Admin from "./pages/Admin";
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
-
-function Home() {
-  const handleLogin = async (credentialResponse) => {
-    try {
-      const token = credentialResponse.credential;
-      localStorage.setItem("google_token", token);
-
-      const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/auth/google`,
-        {
-          token,
-        }
-      );
-
-      alert(`Bienvenido ${res.data.nombre}`);
-    } catch (error) {
-      alert(error.response?.data?.message || "Error al autenticar");
-    }
-  };
-
-  return (
-    <div style={{ marginTop: "5rem", textAlign: "center" }}>
-      <h2>Iniciar sesión con cuenta institucional</h2>
-      <GoogleLogin
-        onSuccess={handleLogin}
-        onError={() => alert("Error en el login")}
-      />
-    </div>
-  );
-}
+import Landing from "./pages/Landing";
 
 function App() {
   return (
