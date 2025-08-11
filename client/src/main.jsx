367db8b400be1485e9dd4ed453752037f18a25ea
Leandro Kevin Monaco <monacoleandrokevin@gmail.com>
2025-08-10 21:35:36 -0300

    AUTHPROVIDER

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import App from "./App";
+import App from "./App.jsx";
+import AuthProvider from "./context/AuthProvider.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import AuthProvider from "./context/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <AuthProvider>
       <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <App />
       </BrowserRouter>
-    </GoogleOAuthProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
