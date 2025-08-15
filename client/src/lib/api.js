d9060eda95460f4878c47dc3350cf9b8a6ef1367
Leandro Kevin Monaco <monacoleandrokevin@gmail.com>
2025-08-14 21:59:20 -0300

    arregloapijs

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -6,9 +6,24 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("session_token"); // <- CLAVE CORRECTA
+  const token = localStorage.getItem("session_token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    const status = err?.response?.status;
+    if (status === 401) {
+      // sesión inválida/expirada: limpiar y dirigir al login
+      localStorage.removeItem("session_token");
+      localStorage.removeItem("usuario_nombre");
+      localStorage.removeItem("usuario_rol");
+      if (location.pathname !== "/") location.replace("/");
+    }
+    return Promise.reject(err);
+  }
+);
 
 export default api;
